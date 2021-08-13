ed4590f4e0e404e4d5a841bcc3510e50c94e4fd1
liudeshun <admin@w3h5.com>
2021-08-14 00:49:07 +0800

    网站列表

---
+++ src/config/site.ts
@@ -0,0 +1,136 @@
+import { SiteListModel } from '../types/siteModel'
+
+const websites: SiteListModel[] = [
+  {
+    cate: "搜索引擎",
+    sites: [
+      {
+        name: "Google",
+        url: "https://www.google.com",
+        target: "_blank",
+        desc: "Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking ..."
+      },
+      {
+        name: "百度搜索",
+        url: "https://www.baidu.com",
+        target: "_blank",
+        desc: "百度一下，你就知道！"
+      },
+      {
+        name: "360搜索",
+        url: "https://www.so.com",
+        target: "_blank",
+        desc: "SO 靠谱"
+      },
+      {
+        name: "搜狗搜索",
+        url: "https://www.sogou.com",
+        target: "_blank",
+        desc: "上网从搜狗开始"
+      }
+    ]
+  },
+  {
+    cate: "技术博客",
+    sites: [
+      {
+        name: "前端资源网",
+        url: "https://www.w3h5.com",
+        target: "_blank",
+        desc: "前端资源网(www.w3h5.com),专门搜集整理前端工作中遇到的各种问题、解决方案、新技术和热门工具插件等。"
+      },
+      {
+        name: "阮一峰",
+        url: "http://www.ruanyifeng.com/blog/",
+        target: "_blank",
+        desc: "阮一峰的网络日志"
+      },
+      {
+        name: "廖雪峰",
+        url: "https://www.liaoxuefeng.com",
+        target: "_blank",
+        desc: "研究互联网产品和技术，提供原创中文精品教程。"
+      },
+      {
+        name: "王垠",
+        url: "https://www.yinwang.org",
+        target: "_blank",
+        desc: "当然我在扯淡"
+      },
+      {
+        name: "张戈",
+        url: "https://zhangge.net",
+        target: "_blank",
+        desc: "关注IT运维技术及互联网的个人博客"
+      },
+      {
+        name: "酷壳",
+        url: "https://coolshell.cn",
+        target: "_blank",
+        desc: "享受编程和技术所带来的快乐"
+      },
+      {
+        name: "卢松松",
+        url: "https://lusongsong.com",
+        target: "_blank",
+        desc: "为草根创业者提供网络推广知识"
+      },
+    ]
+  },
+  {
+    cate: "工具&组件库",
+    sites: [
+      {
+        name: "Ant Design",
+        url: "https://ant.design/",
+        target: "_blank",
+        desc: "Ant Design，企业级产品设计体系，创造高效愉悦的工作体验。"
+      },
+      {
+        name: "Ant Design Pro",
+        url: "https://pro.ant.design/",
+        target: "_blank",
+        desc: "Ant Design Pro，开箱即用的中台前端/设计解决方案。"
+      },
+      {
+        name: "Element UI",
+        url: "https://element.eleme.io/",
+        target: "_blank",
+        desc: "Element，一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库。"
+      },
+      {
+        name: "Element Plus",
+        url: "https://element-plus.org/",
+        target: "_blank",
+        desc: "Element Plus，一套为开发者、设计师和产品经理准备的基于 Vue 3.0 的桌面端组件库"
+      },
+      {
+        name: "Babel",
+        url: "https://babel.dev/",
+        target: "_blank",
+        desc: "JS编译"
+      },
+      {
+        name: "Sioe",
+        url: "https://sioe.cn",
+        target: "_blank",
+        desc: "RGB转换"
+      },
+      {
+        name: "uCharts",
+        url: "https://lusongsong.com",
+        target: "_blank",
+        desc: "高性能跨全端图表"
+      },
+      {
+        name: "draw.io",
+        url: "https://draw.io",
+        target: "_blank",
+        desc: "画图工具"
+      }
+    ]
+  }
+]
+
+
+export  default websites
